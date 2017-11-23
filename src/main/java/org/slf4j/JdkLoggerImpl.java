23e3ffc1dd53f6ea0e07edab52968d408b999545
Sebastian Kürten <sebastian@topobyte.de>
2017-11-23 15:47:27 +0100

    Disable filling in caller data
    LogRecord's setSourceClassName() and getSourceMethodName() are not
    supported on GWT, so drop this feature.

--- src/main/java/org/slf4j/JdkLoggerImpl.java
+++ src/main/java/org/slf4j/JdkLoggerImpl.java
@@ -646,49 +646,11 @@ public final class JdkLoggerImpl extends MarkerIgnoringBase
 		LogRecord record = new LogRecord(level, msg);
 		record.setLoggerName(getName());
 		record.setThrown(t);
-		fillCallerData(callerFQCN, record);
 		logger.log(record);
 
 	}
 
 	static String SELF = JdkLoggerImpl.class.getName();
 	static String SUPER = MarkerIgnoringBase.class.getName();
-
-	/**
-	 * Fill in caller data if possible.
-	 * 
-	 * @param record
-	 *            The record to update
-	 */
-	final private void fillCallerData(String callerFQCN, LogRecord record)
-	{
-		StackTraceElement[] steArray = new Throwable().getStackTrace();
-
-		int selfIndex = -1;
-		for (int i = 0; i < steArray.length; i++) {
-			final String className = steArray[i].getClassName();
-			if (className.equals(callerFQCN) || className.equals(SUPER)) {
-				selfIndex = i;
-				break;
-			}
-		}
-
-		int found = -1;
-		for (int i = selfIndex + 1; i < steArray.length; i++) {
-			final String className = steArray[i].getClassName();
-			if (!(className.equals(callerFQCN) || className.equals(SUPER))) {
-				found = i;
-				break;
-			}
-		}
-
-		if (found != -1) {
-			StackTraceElement ste = steArray[found];
-			// setting the class name has the side effect of setting
-			// the needToInferCaller variable to false.
-			record.setSourceClassName(ste.getClassName());
-			record.setSourceMethodName(ste.getMethodName());
-		}
-	}
 
 }
